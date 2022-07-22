9fdcb9bb954301e2cb815303239c32bc9fb7304e
FannyKaroliine <stephanie.karoline@live.com>
2022-07-22 15:14:40 -0300

    exercicios de sobrecarga em java

---
+++ src/ExercicioSobrecargaJava/ExercicioSobrecargaJava.java
@@ -0,0 +1,14 @@
+package ExercicioSobrecargaJava;
+
+public class ExercicioSobrecargaJava {
+    public static void main(String[] args) {
+
+        System.out.println("Exercício de quadriláteros");
+
+        Quadrilatero.area(5);
+        Quadrilatero.area(10, 25);
+        Quadrilatero.area(6, 13, 10)
+
+    }
+
+}
